aa85f587c4e3c547d6cfabebb1751898a7e599df
joaquinflores23 <joakoflores@hotmail.com>
2023-07-17 14:00:14 -0300

    tarea clase 1

---
+++ clase01/main.js
@@ -0,0 +1,31 @@
+console.log('Hola Mama, estoy programando');
+
+let numeroPar = 12;
+
+let numeroImpar = 99;
+
+console.log(numeroPar + numeroImpar);
+
+let usuario = prompt('Ingrese su nombre de Usuario');
+
+console.log(`Bienvenide a nuestra primera web ${usuario}`);
+
+let multi = 5 * numeroImpar;
+
+console.log(multi);
+
+let confirmEdad = prompt('Eres mayor de edad?');
+
+let resta = multi - 100;
+
+console.log(resta);
+
+console.log(typeof numeroImpar);
+
+console.log(typeof usuario);
+
+let edad = Number(prompt('Ingrese su edad'));
+
+let añoActual = new Date().getFullYear();
+
+alert(`Naciste en el año ${añoActual - edad}`);
